e2c8b85b3447f9ed65761229f1065250b84f3816
shakamoushie <88389632+shakamoushie@users.noreply.github.com>
2023-12-08 13:36:58 +0530

    Add files via upload

--- ss.py
+++ ss.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from st_screen_stats import ScreenData     # pip install streamlit-screen-stats
 
-screenD = ScreenData(setTimeout=1000)
-screen_d = screenD.st_screen_data_window_top()
-st.write(f"screen_d: {screen_d}")
+
+def DetermineLoginDevice():
+    screenD = ScreenData(setTimeout=1000)
+    screen_stats = screenD.st_screen_data_window_top()
+
+    return "mobile" if screen_stats['screen']['width'] <= 400 or screen_stats['screen']['height'] <= 400 else "laptop"
